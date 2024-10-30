9d1091e3354250e2c3f64ceefa55efa5a7f6b734
mahasiswa229 <mafaqih1945@gmail.com>
2024-10-30 10:41:47 +0700

    ,

--- class.py
+++ class.py
@@ -12,14 +12,14 @@ class PersegiPanjang:
     def __str__(self):
         return f'Persegi panjang, panjang {self.panjang} cm, dan lebar {self.lebar} cm'
 
-# Input dari pengguna
+
 panjang = float(input("Masukkan panjang persegi panjang (cm): "))
 lebar = float(input("Masukkan lebar persegi panjang (cm): "))
 
-# Membuat objek PersegiPanjang
+
 persegi_panjang = PersegiPanjang(panjang, lebar)
 
-# Menampilkan informasi
+
 print(persegi_panjang)
 print(f'Keliling: {persegi_panjang.hitung_keliling()} cm')
 print(f'Luas: {persegi_panjang.hitung_luas()} cm²')
